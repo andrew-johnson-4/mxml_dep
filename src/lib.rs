a82955f543abd02f420ebee45bbd6613febe9e9a
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-19 13:14:40 -0700

    xml parse/unparse working

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,68 @@
 
-use html_parser::{Dom, Result};
+use html_parser::{Dom, Node, Result, Element, ElementVariant};
 
 pub fn parse(es: &str) -> Result<Dom> {
    Dom::parse(es)
+}
+
+pub fn unparse(d: &Dom) -> String {
+   let mut es = String::new();
+   for c in d.children.iter() {
+      es.push_str(&unparse_node(c));
+   }
+   es
+}
+
+pub fn unparse_node(n: &Node) -> String {
+   match n {
+      Node::Text(s) => { s.clone() },
+      Node::Element(e) => {
+         unparse_element(e)
+      },
+      Node::Comment(_) => { "".to_string() }
+   }
+}
+
+pub fn unparse_element(e: &Element) -> String {
+   let mut es = String::new();
+
+   es.push_str("<");
+   es.push_str(&e.name);
+
+   if let Some(ref id) = e.id {
+      es.push_str(&format!(r#" id="{}""#, id));
+   }
+
+   if e.classes.len()>0 {
+      es.push_str(&format!(r#" class="{}""#, e.classes.join(" ")));
+   }
+
+   for (k,v) in e.attributes.iter() {
+      if let Some(v) = v {
+         es.push_str(&format!(r#" {}="{}""#, k, v.replace("\"","\\\"")));
+      } else {
+         es.push_str(&format!(" {}", k));
+      }
+   }
+
+   match e.variant {
+      ElementVariant::Normal => {
+         es.push_str(">");
+         for c in e.children.iter() {
+            es.push_str(&unparse_node(c));
+         }
+      },
+      ElementVariant::Void => {}
+   }
+
+   match e.variant {
+      ElementVariant::Normal => {
+         es.push_str(&format!("</{}>", e.name));
+      },
+      ElementVariant::Void => {
+         es.push_str("/>");
+      }
+   }
+
+   es
 }
--- tests/parse.rs
+++ tests/parse.rs
@@ -8,3 +8,30 @@ fn parse1() {
       r#"{"treeType":"documentFragment","children":[{"name":"br","variant":"void"}]}"#
    );
 }
+
+#[test]
+fn parse2() {
+   let xml = parse("<br/>").unwrap();
+   assert_eq!(
+      unparse(&xml),
+      "<br/>"
+   );
+}
+
+#[test]
+fn parse3() {
+   let xml = parse("<div>dave david</div>").unwrap();
+   assert_eq!(
+      unparse(&xml),
+      "<div>dave david</div>"
+   );
+}
+
+#[test]
+fn parse4() {
+   let xml = parse(r#"<div id="a"><b class="b">dave</b></div>"#).unwrap();
+   assert_eq!(
+      unparse(&xml),
+      r#"<div id="a"><b class="b">dave</b></div>"#
+   );
+}

bf19f4b8fe2904aae90126470142e8977468ff61
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-06-20 11:13:05 -0700

    test nested selection criteria

---
+++ tests/select.rs
@@ -0,0 +1,43 @@
+use mxml_dep::*;
+
+#[test]
+fn parse1() {
+   let mut xml = parse("<p>a<p>b<p>c</p></p></p>").unwrap();
+   let m = FindMatchEditElement {
+      fme: vec![(
+         FindElement{find:vec![]},
+         MatchElement{when:vec![Match::HasTag("p".to_string())]},
+         EditElement{edit:vec![Edit::AddAttribute("a".to_string(),"".to_string())]}
+      ),(
+         FindElement{find:vec![]},
+         MatchElement{when:vec![Match::HasTag("p".to_string())]},
+         EditElement{edit:vec![Edit::AddAttribute("b".to_string(),"".to_string())]}
+      )]
+   };
+   fme(&mut xml, &m);
+   assert_eq!(
+      unparse(&xml),
+      r#"<p a>a<p b>b<p>c</p></p></p>"#
+   );
+}
+
+#[test]
+fn parse2() {
+   let mut xml = parse("<p>a<p a>b<p>c</p></p></p>").unwrap();
+   let m = FindMatchEditElement {
+      fme: vec![(
+         FindElement{find:vec![]},
+         MatchElement{when:vec![Match::HasAttribute("a".to_string())]},
+         EditElement{edit:vec![]},
+      ),(
+         FindElement{find:vec![]},
+         MatchElement{when:vec![Match::HasTag("p".to_string())]},
+         EditElement{edit:vec![Edit::AddAttribute("b".to_string(),"".to_string())]}
+      )]
+   };
+   fme(&mut xml, &m);
+   assert_eq!(
+      unparse(&xml),
+      r#"<p>a<p a>b<p b>c</p></p></p>"#
+   );
+}
